ea68a4347c19c9c03a4252f78ccdb973285ee823
HR-SU <52516894+HR-SU@users.noreply.github.com>
2021-01-07 14:47:13 +0800

    Update main.cpp

--- PCLogo-demo/PCLogo-test/main.cpp
+++ PCLogo-demo/PCLogo-test/main.cpp
@@ -10,10 +10,10 @@
 int main(int argc, char *argv[])
 {
     TestRunner tr;
-//    tr.addTest(new TestLineInterpreter);
-//    tr.addTest(new TestStartWindow);
-//    tr.addTest(new TestModeSelect);
-//    tr.addTest(new TestLocalMode);
+    tr.addTest(new TestLineInterpreter);
+    tr.addTest(new TestStartWindow);
+    tr.addTest(new TestModeSelect);
+    tr.addTest(new TestLocalMode);
     tr.addTest(new TestAdvancedChat);
     tr.addTest(new TestChat);
     tr.addTest(new TestNetMode);
