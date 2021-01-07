dbbd7512c7e08b023498766b1b2c63085c5cfef5
Lycheenut <53185250+Lycheenut@users.noreply.github.com>
2021-01-07 19:37:26 +0800

    Update main.cpp

--- PCLogo-demo/PCLogo-test/main.cpp
+++ PCLogo-demo/PCLogo-test/main.cpp
@@ -13,14 +13,14 @@ int main(int argc, char *argv[])
 {
     TestRunner tr;
     tr.addTest(new TestLineInterpreter);
-//    tr.addTest(new TestStartWindow);
-//    tr.addTest(new TestModeSelect);
-//    tr.addTest(new TestLocalMode);
-//    tr.addTest(new TestNewLine);
-//    tr.addTest(new TestCmdLine);
-//    tr.addTest(new TestAdvancedChat);
-//    tr.addTest(new TestChat);
-//    tr.addTest(new TestNetMode);
+    tr.addTest(new TestStartWindow);
+    tr.addTest(new TestModeSelect);
+    tr.addTest(new TestLocalMode);
+    tr.addTest(new TestNewLine);
+    tr.addTest(new TestCmdLine);
+    tr.addTest(new TestAdvancedChat);
+    tr.addTest(new TestChat);
+    tr.addTest(new TestNetMode);
 
     bool pass = tr.runTests(argc, argv);
     qDebug() << "Overall result: " << (pass ? "PASS" : "FAIL");
